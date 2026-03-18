cfba927b5c826c4d7a5a8daa9830ef1df4adf203
S K KAVINRAJ <126429868+kavinraj710@users.noreply.github.com>
2026-03-18 19:46:34 +0530

    Add EventBridge target and permissions for Lambda

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -5,6 +5,7 @@ resource "aws_cloudwatch_event_rule" "monthly" {
 
 resource "aws_cloudwatch_event_target" "lambda_target" {
   rule = aws_cloudwatch_event_rule.monthly.name
+  target_id = "lambda-target"
   arn  = aws_lambda_function.snapshot_cleaner.arn
 }
 
@@ -13,4 +14,5 @@ resource "aws_lambda_permission" "allow_eventbridge" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.snapshot_cleaner.function_name
   principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.monthly.arn
 }
