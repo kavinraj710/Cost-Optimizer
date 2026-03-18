197a8f1227f7b77c062d48ad89d8cbd4ae4fae74
S K KAVINRAJ <126429868+kavinraj710@users.noreply.github.com>
2026-03-18 19:55:26 +0530

    Create IAM role and policy for Lambda snapshot
    Added IAM role and policy for Lambda function.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,20 @@
+# IAM Role
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_snapshot_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# IAM Policy attached to Role
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "lambda_snapshot_policy"
   role = aws_iam_role.lambda_role.id
